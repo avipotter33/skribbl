d8893f8d39a83d04c241684f6b6848cb101066dc
lihi7 <lihi7878@gmail.com>
2023-03-09 19:46:22 +0200

    added server and client

--- Client.py
+++ Client.py
@@ -11,24 +11,98 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect to the server
 client_socket.connect((HOST, PORT))
 
-# Receive screen size from server
-screen_size = pickle.loads(client_socket.recv(1024))
+# get player index
+player_index = client_socket.recv(1024)
 
-# Set up Pygame screen
-pygame.init()
-screen = pygame.display.set_mode(screen_size)
+# set the window size and title
+# TODO: in constants
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Draw on the Screen")
+
+# set up the colors
+# TODO: in constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# set up the drawing surface
+drawing_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+drawing_surface.fill(WHITE)
+
+# set up the drawing tools
+# TODO: in constants
+brush_size = 5
+brush_color = BLACK
+
+clicked_save = False
+got_drawing = False
 
 # start the game loop
-while True:
-    # get the player's move
-    move = input("Enter your move (rock, paper, scissors): ")
+running = True
+if player_index == 0:
+    while running:
+        # handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # left mouse button
+                    mouse_y_pos = pygame.mouse.get_pos()[1]
+                    mouse_x_pos = pygame.mouse.get_pos()[0]
+                    if 20 < mouse_x_pos < 20 + 50 and 50 < mouse_y_pos < 100:
+                        pygame.image.save(drawing_surface, os.path.join("screenshots", f"screenshot.png"))
+                        clicked_save = True
+                    else:
+                        pygame.draw.circle(drawing_surface, brush_color, event.pos, brush_size)
+                        last_pos = event.pos
+                elif event.button == 3:  # right mouse button
+                    # undo last path
+                    drawing_surface.fill(WHITE)
+            elif event.type == pygame.MOUSEMOTION:
+                if event.buttons[0]:  # left mouse button
+                    pygame.draw.line(drawing_surface, brush_color, last_pos, event.pos, brush_size)
+                    last_pos = event.pos
 
-    # send the move to the server
-    client_socket.send(move.encode())
+        rect = pygame.Rect(20, 50, 50, 50)
+        pygame.draw.rect(drawing_surface, BLACK, rect)
 
-    # receive the result from the server
-    result = client_socket.recv(1024)
-    print(result.decode())
+        # update the screen
+        window.blit(drawing_surface, (0, 0))
+        pygame.display.flip()
 
-# close the connection
-client
+        if clicked_save:
+            # Read the image data from a file
+            with open("screenshots/screenshot.png", "rb") as f:
+                image_to_bytes = f.read()
+
+            # send the move to the server
+            client_socket.send(image_to_bytes)
+
+            # receive the result from the server
+            result = client_socket.recv(1024)
+
+            print(result.decode())
+
+            # Make sure to only update this client's screen
+            client_socket.sendall(b'update')
+
+            clicked_save = False
+else:
+    while running:
+        if not got_drawing:
+            image_data = client_socket.recv(1024)
+            with open(os.path.join("saved_drawings", f"image.png"),"wb") as f:
+                f.write(image_data)
+            image = pygame.image.load(os.path.join("saved_drawings", f"image.png"))
+            image = pygame.transform.scale(image,(WINDOW_WIDTH ,WINDOW_HEIGHT))
+            got_drawing = True
+
+        # update the screen
+        window.blit(image,(0,0))
+        pygame.display.flip()
+
+# quit Pygame
+pygame.quit()
+
--- Server.py
+++ Server.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import pickle
 
 HOST = ''  # listen on all available network interfaces
 PORT = 5000  # use a non-reserved port number
@@ -20,40 +22,11 @@ print(f"Player 1 connected from {address1}")
 player2, address2 = server_socket.accept()
 print(f"Player 2 connected from {address2}")
 
-# send screen to each player
-player1.send(100)
-player2.send(100)
-
-# start the game loop
 while True:
-    # receive a move from player 1
-    move1 = player1.recv(1024)
-    if not move1:
-        print("Player 1 disconnected")
-        break
-
-    # receive a move from player 2
-    move2 = player2.recv(1024)
-    if not move2:
-        print("Player 2 disconnected")
-        break
-
-    # determine the winner and send the result to each player
-    if move1 == b"rock" and move2 == b"scissors":
-        player1.send(b"You win!")
-        player2.send(b"You lose!")
-    elif move1 == b"paper" and move2 == b"rock":
-        player1.send(b"You win!")
-        player2.send(b"You lose!")
-    elif move1 == b"scissors" and move2 == b"paper":
-        player1.send(b"You win!")
-        player2.send(b"You lose!")
-    elif move1 == move2:
-        player1.send(b"It's a tie!")
-        player2.send(b"It's a tie!")
-    else:
-        player1.send(b"You lose!")
-        player2.send(b"You win!")
+    # Receive the image data from player0
+    data = player1.recv(1024)
+    player1.send(b'ok')
+    player2.send(data)
 
 # close the connections and the socket object
 player1.close()
