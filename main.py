bde772d706e4ee58ec2a39103c8a56b345da2350
Aiiiiiiii <avia111206@gmail.com>
2023-03-09 18:28:03 +0200

    added screenshot button

--- main.py
+++ main.py
@@ -33,8 +33,13 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # left mouse button
-                pygame.draw.circle(drawing_surface, brush_color, event.pos, brush_size)
-                last_pos = event.pos
+                mouse_y_pos = pygame.mouse.get_pos()[1]
+                mouse_x_pos = pygame.mouse.get_pos()[0]
+                if 20 < mouse_x_pos < 20 + 50 and 50 < mouse_y_pos < 100:
+                    pygame.image.save(drawing_surface, os.path.join("screenshots", "screenshot.png"))
+                else:
+                    pygame.draw.circle(drawing_surface, brush_color, event.pos, brush_size)
+                    last_pos = event.pos
             elif event.button == 3:  # right mouse button
                 # undo last path
                 drawing_surface.fill(WHITE)
@@ -43,7 +48,8 @@ while running:
                 pygame.draw.line(drawing_surface, brush_color, last_pos, event.pos, brush_size)
                 last_pos = event.pos
 
-
+    rect = pygame.Rect(20, 50, 50, 50)
+    pygame.draw.rect(drawing_surface, BLACK, rect)
 
     # update the screen
     window.blit(drawing_surface, (0, 0))
