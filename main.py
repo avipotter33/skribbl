9b40ba767ed72cd0348515c82d164ddc5141f8b3
lihi7 <lihi7878@gmail.com>
2023-03-09 17:28:26 +0200

    added server and client

--- main.py
+++ main.py
@@ -1,45 +1,58 @@
-from Constants import *
 import pygame
-from helpers import *
 
+# initialize Pygame
+pygame.init()
 
-def main():
-    # Set up the game display, clock and headline
-    pygame.init()
+# set the window size and title
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Draw on the Screen")
 
-    # Change the title of the window
-    pygame.display.set_caption('Skribbl')
+# set up the colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-    clock = pygame.time.Clock()
+# set up the drawing surface
+drawing_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+drawing_surface.fill(WHITE)
 
-    # Set up background image
-    background = pygame.image.load('background image.jpg')
-    background = pygame.transform.scale(background,
-                                        (WINDOW_WIDTH, WINDOW_HEIGHT))
+# set up the drawing tools
+brush_size = 5
+brush_color = BLACK
 
-    running = True
-    while running:
-        # Grabs events such as key pressed, mouse pressed and so.
-        # Going through all the events that happened in the last clock tick
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+# set up the undo button
+undo_button = pygame.Rect(10, 10, 50, 30)
+undo_font = pygame.font.SysFont(None, 24)
+undo_text = undo_font.render("Undo", True, BLACK)
 
-            pygame.display.update()
-            # Display the background, presented Image, likes, comments, tags and
-            # location(on the Image)
-            screen.fill(BLACK)
-            screen.blit(background, (0, 0))
+# start the game loop
+running = True
+while running:
+    # handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # left mouse button
+                pygame.draw.circle(drawing_surface, brush_color, event.pos, brush_size)
+                last_pos = event.pos
+            elif event.button == 3:  # right mouse button
+                # undo last path
+                drawing_surface.fill(WHITE)
+        elif event.type == pygame.MOUSEMOTION:
+            if event.buttons[0]:  # left mouse button
+                pygame.draw.line(drawing_surface, brush_color, last_pos, event.pos, brush_size)
+                last_pos = event.pos
 
-            # Update display - without input update everything
-            pygame.display.update()
+    # draw the undo button
+    pygame.draw.rect(drawing_surface, WHITE, undo_button)
+    pygame.draw.rect(drawing_surface, BLACK, undo_button, 1)
+    drawing_surface.blit(undo_text, (undo_button.x + 5, undo_button.y + 5))
 
-            # Set the clock tick to be 60 times per second. 60 frames for second.
-            # If we want faster game - increase the parameter.
-            clock.tick(60)
+    # update the screen
+    window.blit(drawing_surface, (0, 0))
+    pygame.display.flip()
 
-    pygame.quit()
-    quit()
-
-
-main()
+# quit Pygame
+pygame.quit()
