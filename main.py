9923db8bb7491fa04a89ec9a0d5e3a45935cf963
Aiiiiiiii <avia111206@gmail.com>
2023-03-09 17:50:41 +0200

    added buttons

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import pygame
 from helpers import *
 
 
+def add_image(img_path, x_pos, y_pos, width, height, screen):
+    img = pygame.image.load(img_path)
+    img = pygame.transform.scale(img, (width, height))
+    screen.blit(img, (x_pos, y_pos))
+
+
 def main():
     # Set up the game display, clock and headline
     pygame.init()
@@ -22,6 +28,8 @@ def main():
     button1 = pygame.image.load('button.png')
     button1 = pygame.transform.scale(button1, (BUTTON_WIDTH, BUTTON_HEIGHT))
 
+    add_icons = False
+
     running = True
     while running:
         # Grabs events such as key pressed, mouse pressed and so.
@@ -29,13 +37,41 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_y_pos = pygame.mouse.get_pos()[1]
+                mouse_x_pos = pygame.mouse.get_pos()[0]
+                if BUTTON1_X_POS < mouse_x_pos < BUTTON1_X_POS + BUTTON_WIDTH and BUTTON1_Y_POS * 1.8 < mouse_y_pos < BUTTON1_Y_POS * 1.8 + BUTTON_HEIGHT:
+                    if add_icons:
+                        add_icons = False
+                    else:
+                        add_icons = True
 
             pygame.display.update()
             # Display the background, presented Image, likes, comments, tags and
             # location(on the Image)
             screen.fill(WHITE)
             screen.blit(background, rect)
-            screen.blit(button1, (BUTTON1_X_POS, BUTTON1_Y_POS))
+            add_image('play button.png', BUTTON1_X_POS, BUTTON1_Y_POS, BUTTON_WIDTH, BUTTON_HEIGHT, screen)
+            add_image('button.png', BUTTON1_X_POS, BUTTON1_Y_POS * 1.8, BUTTON_WIDTH - 10, BUTTON_HEIGHT, screen)
+            add_image('thumbnail.png', THUMBNAIL_X_POS, THUMBNAIL_Y_POS, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT, screen)
+
+            mouse_y_pos = pygame.mouse.get_pos()[1]
+            mouse_x_pos = pygame.mouse.get_pos()[0]
+
+            if BUTTON1_X_POS < mouse_x_pos < BUTTON1_X_POS + BUTTON_WIDTH and BUTTON1_Y_POS < mouse_y_pos < BUTTON1_Y_POS + BUTTON_HEIGHT:
+                add_image('play button bold.png', BUTTON1_X_POS - 8, BUTTON1_Y_POS - 14, BUTTON_WIDTH + 30, BUTTON_HEIGHT + 12, screen)
+
+            if BUTTON1_X_POS < mouse_x_pos < BUTTON1_X_POS + BUTTON_WIDTH and BUTTON1_Y_POS * 1.8 < mouse_y_pos < BUTTON1_Y_POS * 1.8 + BUTTON_HEIGHT:
+                add_image('topics button bold.png', BUTTON1_X_POS - 16, BUTTON1_Y_POS * 1.8 - 6, BUTTON_WIDTH + 13, BUTTON_HEIGHT + 13, screen)
+
+            if add_icons:
+                add_image('music icon.png', ICON_BUTTON_X_POS, ICON_BUTTON_Y_POS, ICON_BUTTON_WIDTH,
+                          ICON_BUTTON_HEIGHT, screen)
+                add_image('sports icon.png', ICON_BUTTON_X_POS + 250, ICON_BUTTON_Y_POS, ICON_BUTTON_WIDTH - 10,
+                          BUTTON_HEIGHT, screen)
+                add_image('movies icon.png', ICON_BUTTON_X_POS + 250 * 2, ICON_BUTTON_Y_POS, ICON_BUTTON_WIDTH - 10,
+                          BUTTON_HEIGHT,
+                          screen)
 
             # Update display - without input update everything
             pygame.display.update()
