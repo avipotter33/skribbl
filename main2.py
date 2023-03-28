1bf08a594f65dcc91ed49c1a035a338a9d355106
im0804 <95828796+im0804@users.noreply.github.com>
2023-03-28 17:38:19 +0300

    After sorting for classes

--- main2.py
+++ main2.py
@@ -7,83 +7,83 @@ from buttons import *
 import translators as ts
 from drawing_player import *
 
-change_language = True
-PLAY_TEXT = ts.translate_text(PLAY_TEXT, to_language='he')
+
+class home_screen:
+    def __init__(self):
+        change_language = True
+        PLAY_TEXT = ts.translate_text(PLAY_TEXT, to_language='he')
+        play_button = buttons.play_button
+
+    def add_image(img_path, x_pos, y_pos, width, height, screen):
+        img = pygame.image.load(img_path)
+        img = pygame.transform.scale(img, (width, height))
+        screen.blit(img, (x_pos, y_pos))
 
 
-def add_image(img_path, x_pos, y_pos, width, height, screen):
-    img = pygame.image.load(img_path)
-    img = pygame.transform.scale(img, (width, height))
-    screen.blit(img, (x_pos, y_pos))
+    def main():
+        screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
+        # Set up the game display, clock and headline
+        pygame.init()
 
+        # Change the title of the window
+        pygame.display.set_caption('Skribbl')
 
-play_button = buttons.play_button
+        clock = pygame.time.Clock()
 
+        # Set up background image
+        background = pygame.image.load('Images/background_image.jpg')
+        background = pygame.transform.scale(background,
+                                            (WINDOW_WIDTH, WINDOW_HEIGHT))
 
-def main():
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE)
-    # Set up the game display, clock and headline
-    pygame.init()
+        image_x_index = 0
 
-    # Change the title of the window
-    pygame.display.set_caption('Skribbl')
+        pygame.mouse.set_visible(False)
+        cursor_img = pygame.image.load('Images/cursor_image.png')
+        cursor_img = pygame.transform.scale(cursor_img,
+                                            (CURSOR_WIDTH, CURSOR_HEIGHT))
+        cursor_img_rect = cursor_img.get_rect()
 
-    clock = pygame.time.Clock()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
+                    if mouse_in_button(play_button, mouse_pos):
+                        dp_main()
+            screen.fill(BLUE)
+            screen.blit(background, (image_x_index, 0))
 
-    # Set up background image
-    background = pygame.image.load('Images/background_image.jpg')
-    background = pygame.transform.scale(background,
-                                        (WINDOW_WIDTH, WINDOW_HEIGHT))
+            if image_x_index != (0 - WINDOW_WIDTH):
+                screen.blit(background, (WINDOW_WIDTH + image_x_index, 0))
+            else:
+                screen.blit(background, (WINDOW_WIDTH + image_x_index, 0))
+                image_x_index = 0
 
-    image_x_index = 0
+            image_x_index -= 1
 
-    pygame.mouse.set_visible(False)
-    cursor_img = pygame.image.load('Images/cursor_image.png')
-    cursor_img = pygame.transform.scale(cursor_img,
-                                        (CURSOR_WIDTH, CURSOR_HEIGHT))
-    cursor_img_rect = cursor_img.get_rect()
+            add_image('Images/home page5(2).png', HOME_PAGE_X_POS, HOME_PAGE_Y_POS, HOME_PAGE_WIDTH, HOME_PAGE_HEIGHT, screen)
+            add_image('Images/button.png', PLAY_BTN_X_POS, PLAY_BTN_Y_POS, PLAY_BTN_WIDTH, PLAY_BTN_HEIGHT, screen)
+            #add_image('Images/yellow button.png', BUTTON_X_POS, BUTTON_Y_POS, BUTTON_WIDTH, BUTTON_HEIGHT, screen)
 
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = event.pos
-                if mouse_in_button(play_button, mouse_pos):
-                    dp_main()
-        screen.fill(BLUE)
-        screen.blit(background, (image_x_index, 0))
+            cursor_img_rect.center = pygame.mouse.get_pos()  # update position
+            if mouse_in_button(play_button, pygame.mouse.get_pos()):
+                add_image('Images/yellow button.png', PLAY_BTN_X_POS, PLAY_BTN_Y_POS, PLAY_BTN_WIDTH, PLAY_BTN_HEIGHT,
+                          screen)
+            font = pygame.font.SysFont('Anything Skribble', PLAY_TEXT_SIZE)
+            text = font.render(PLAY_TEXT, True, WHITE)
+            screen.blit(text, [PLAY_BTN_X_POS + 22, PLAY_BTN_Y_POS])
 
-        if image_x_index != (0 - WINDOW_WIDTH):
-            screen.blit(background, (WINDOW_WIDTH + image_x_index, 0))
-        else:
-            screen.blit(background, (WINDOW_WIDTH + image_x_index, 0))
-            image_x_index = 0
+            screen.blit(cursor_img, cursor_img_rect)  # draw the cursor
 
-        image_x_index -= 1
+            pygame.display.update()
 
-        add_image('Images/home page5(2).png', HOME_PAGE_X_POS, HOME_PAGE_Y_POS, HOME_PAGE_WIDTH, HOME_PAGE_HEIGHT, screen)
-        add_image('Images/button.png', PLAY_BTN_X_POS, PLAY_BTN_Y_POS, PLAY_BTN_WIDTH, PLAY_BTN_HEIGHT, screen)
-        #add_image('Images/yellow button.png', BUTTON_X_POS, BUTTON_Y_POS, BUTTON_WIDTH, BUTTON_HEIGHT, screen)
+            # Set the clock tick to be 60 times per second. 60 frames for second.
+            # If we want faster game - increase the parameter.
+            clock.tick(20)
 
-        cursor_img_rect.center = pygame.mouse.get_pos()  # update position
-        if mouse_in_button(play_button, pygame.mouse.get_pos()):
-            add_image('Images/yellow button.png', PLAY_BTN_X_POS, PLAY_BTN_Y_POS, PLAY_BTN_WIDTH, PLAY_BTN_HEIGHT,
-                      screen)
-        font = pygame.font.SysFont('Anything Skribble', PLAY_TEXT_SIZE)
-        text = font.render(PLAY_TEXT, True, WHITE)
-        screen.blit(text, [PLAY_BTN_X_POS + 22, PLAY_BTN_Y_POS])
-
-        screen.blit(cursor_img, cursor_img_rect)  # draw the cursor
-
-        pygame.display.update()
-
-        # Set the clock tick to be 60 times per second. 60 frames for second.
-        # If we want faster game - increase the parameter.
-        clock.tick(20)
-
-    pygame.quit()
-    quit()
+        pygame.quit()
+        quit()
 
 main()
--- main_player2.py
+++ main_player2.py
@@ -6,89 +6,90 @@ from helpers import *
 from buttons import *
 import drawing_player
 
-
-cursor_img = pygame.image.load('Images/cursor_image.png')
-cursor_img = pygame.transform.scale(cursor_img,
-                                    (CURSOR_WIDTH + 20, CURSOR_HEIGHT))
-cursor_img_rect = cursor_img.get_rect()
-
-
-def add_image(img_path, x_pos, y_pos, width, height, screen):
-    img = pygame.image.load(img_path)
-    img = pygame.transform.scale(img, (width, height))
-    screen.blit(img, (x_pos, y_pos))
+class main_player2(drawing_player):
+    def __init__(self):
+        cursor_img = pygame.image.load('Images/cursor_image.png')
+        cursor_img = pygame.transform.scale(cursor_img,
+                                            (CURSOR_WIDTH + 20, CURSOR_HEIGHT))
+        cursor_img_rect = cursor_img.get_rect()
 
 
-def g_player_main():
-    pygame.init()
-    clock = pygame.time.Clock()
-    base_font = pygame.font.Font(None, 32)
-    user_text = ''
-    input_rect = pygame.Rect(COMMENT_BOX_WIDTH, COMMENT_BOX_HEIGHT, COMMENT_BOX_X_POS, COMMENT_BOX_Y_POS)
-    color_active = pygame.Color('lightskyblue1')
-    color_passive = pygame.Color('lightskyblue3')
-    count = 1
-    rect_list = []
-    text_list = []
-    printing_list = []
+    def add_image(img_path, x_pos, y_pos, width, height, screen):
+        img = pygame.image.load(img_path)
+        img = pygame.transform.scale(img, (width, height))
+        screen.blit(img, (x_pos, y_pos))
 
 
-    active = False
-    pressed_enter = False
-    while not(pressed_enter):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print(pygame.mouse.get_pos())
-                if input_rect.collidepoint(event.pos):
-                    active = True
-                else:
-                    active = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DELETE:
-                    user_text = user_text[:-1]
-                elif event.key == pygame.K_RETURN:
-                    pressed_enter = True
-                else:
-                    user_text += event.unicode
-                    # printing_list.append(event.unicode)
-                    # text_list.insert(count, printing_list)
-                    # if len(printing_list) == LINE_MAX_LENGTH:
-                    #     count += 1
-                    #     printing_list = []
+    def g_player_main():
+        pygame.init()
+        clock = pygame.time.Clock()
+        base_font = pygame.font.Font(None, 32)
+        user_text = ''
+        input_rect = pygame.Rect(COMMENT_BOX_WIDTH, COMMENT_BOX_HEIGHT, COMMENT_BOX_X_POS, COMMENT_BOX_Y_POS)
+        color_active = pygame.Color('lightskyblue1')
+        color_passive = pygame.Color('lightskyblue3')
+        count = 1
+        rect_list = []
+        text_list = []
+        printing_list = []
 
-        screen.fill(WHITE)
-        if active:
-            color = color_active
-        else:
-            color = color_passive
 
-        add_image("images/guessing box.png", CHAT_BTN_X_POS, CHAT_BTN_Y_POS, CHAT_BTN_WIDTH, CHAT_BTN_HEIGHT, screen)
+        active = False
+        pressed_enter = False
+        while not(pressed_enter):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print(pygame.mouse.get_pos())
+                    if input_rect.collidepoint(event.pos):
+                        active = True
+                    else:
+                        active = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DELETE:
+                        user_text = user_text[:-1]
+                    elif event.key == pygame.K_RETURN:
+                        pressed_enter = True
+                    else:
+                        user_text += event.unicode
+                        # printing_list.append(event.unicode)
+                        # text_list.insert(count, printing_list)
+                        # if len(printing_list) == LINE_MAX_LENGTH:
+                        #     count += 1
+                        #     printing_list = []
 
-        input_rect = pygame.Rect(COMMENT_BOX_X_POS, COMMENT_BOX_Y_POS, COMMENT_BOX_WIDTH, COMMENT_BOX_HEIGHT)
-
-        pygame.draw.rect(screen, color, input_rect)
-
-        if active:
-            if len(user_text) <= LINE_MAX_LENGTH:
-            # for text in range(len(text_list)):
-            #     print(text_list[text])
-                text_surface = base_font.render(user_text, True, WHITE)
-                screen.blit(text_surface, (GUESS_X_POS, GUESS_Y_POS))
+            screen.fill(WHITE)
+            if active:
+                color = color_active
             else:
-                text_surface = base_font.render(user_text[0:LINE_MAX_LENGTH], True, WHITE)
-                screen.blit(text_surface, (GUESS_X_POS, GUESS_Y_POS))
+                color = color_passive
 
-        cursor_img_rect.center = (pygame.mouse.get_pos()[0] + 22, pygame.mouse.get_pos()[1] - 40) # update position
-        screen.blit(cursor_img, cursor_img_rect)  # draw the cursor
+            add_image("images/guessing box.png", CHAT_BTN_X_POS, CHAT_BTN_Y_POS, CHAT_BTN_WIDTH, CHAT_BTN_HEIGHT, screen)
 
-        pygame.display.flip()
-        clock.tick(60)
+            input_rect = pygame.Rect(COMMENT_BOX_X_POS, COMMENT_BOX_Y_POS, COMMENT_BOX_WIDTH, COMMENT_BOX_HEIGHT)
 
-    if user_text == ran_Level1:
-        print("yay!")
+            pygame.draw.rect(screen, color, input_rect)
+
+            if active:
+                if len(user_text) <= LINE_MAX_LENGTH:
+                # for text in range(len(text_list)):
+                #     print(text_list[text])
+                    text_surface = base_font.render(user_text, True, WHITE)
+                    screen.blit(text_surface, (GUESS_X_POS, GUESS_Y_POS))
+                else:
+                    text_surface = base_font.render(user_text[0:LINE_MAX_LENGTH], True, WHITE)
+                    screen.blit(text_surface, (GUESS_X_POS, GUESS_Y_POS))
+
+            cursor_img_rect.center = (pygame.mouse.get_pos()[0] + 22, pygame.mouse.get_pos()[1] - 40) # update position
+            screen.blit(cursor_img, cursor_img_rect)  # draw the cursor
+
+            pygame.display.flip()
+            clock.tick(60)
+
+        if user_text == rnd_Level1:
+            print("yay!")
 
 
 #g_player_main()
